ba7049a7b1e2d8b6a323fc6bf0301e753ed7cfb2
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-14 13:54:20 +0000

    Adaptado a los nuevos mensajes de error
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@33410 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/error/ServiceExceptionType.java
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/error/ServiceExceptionType.java
@@ -5,15 +5,15 @@ import org.siemac.metamac.core.common.error.CommonServiceExceptionType;
 public class ServiceExceptionType extends CommonServiceExceptionType {
 
     // Extended Error Codes
-    public static final CommonServiceExceptionType SERVICE_ROLE_NOT_OK      = create("0501", "exception.service.role.not_ok");
-    public static final CommonServiceExceptionType SERVICE_ROLE_NOT_FOUND   = create("0502", "exception.service.role.not_found");
+    public static final CommonServiceExceptionType SERVICE_ROLE_NOT_OK      = create("1001", "exception.service.role.not_ok");
+    public static final CommonServiceExceptionType SERVICE_ROLE_NOT_FOUND   = create("1002", "exception.service.role.not_found");
 
-    public static final CommonServiceExceptionType SERVICE_APP_NOT_OK       = create("0601", "exception.service.app.not_ok");
-    public static final CommonServiceExceptionType SERVICE_APP_NOT_FOUND    = create("0602", "exception.service.app.not_found");
+    public static final CommonServiceExceptionType SERVICE_APP_NOT_OK       = create("1101", "exception.service.app.not_ok");
+    public static final CommonServiceExceptionType SERVICE_APP_NOT_FOUND    = create("1102", "exception.service.app.not_found");
 
-    public static final CommonServiceExceptionType SERVICE_USER_NOT_OK      = create("0701", "exception.service.user.not_ok");
-    public static final CommonServiceExceptionType SERVICE_USER_NOT_FOUND   = create("0702", "exception.service.user.not_found");
+    public static final CommonServiceExceptionType SERVICE_USER_NOT_OK      = create("1201", "exception.service.user.not_ok");
+    public static final CommonServiceExceptionType SERVICE_USER_NOT_FOUND   = create("1202", "exception.service.user.not_found");
 
-    public static final CommonServiceExceptionType SERVICE_ACCESS_NOT_OK    = create("0801", "exception.service.access.not_ok");
-    public static final CommonServiceExceptionType SERVICE_ACCESS_NOT_FOUND = create("0802", "exception.service.access.not_found");
+    public static final CommonServiceExceptionType SERVICE_ACCESS_NOT_OK    = create("1301", "exception.service.access.not_ok");
+    public static final CommonServiceExceptionType SERVICE_ACCESS_NOT_FOUND = create("1302", "exception.service.access.not_found");
 }
