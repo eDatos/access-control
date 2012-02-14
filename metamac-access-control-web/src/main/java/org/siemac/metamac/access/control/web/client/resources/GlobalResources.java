52a94052af6929ba086160d3c5c9635a70299423
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-14 09:38:12 +0000

    IN PROGRESS - issue METAMAC-317: Crear proyecto web http://jira.arte-consultores.com/browse/METAMAC-317
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@33368 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/resources/GlobalResources.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/resources/GlobalResources.java
@@ -1,8 +1,7 @@
-package org.siemac.metamac.gopestat.web.client.resources;
+package org.siemac.metamac.access.control.web.client.resources;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.ImageResource;
 
 public interface GlobalResources extends ClientBundle {
 	public static final GlobalResources RESOURCE =  GWT.create(GlobalResources.class);
