c501aa79d689c47789303ab68414ae0d3d275dba
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-14 09:35:52 +0000

    IN PROGRESS - issue METAMAC-317: Crear proyecto web http://jira.arte-consultores.com/browse/METAMAC-317
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@33364 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/gin/ClientModule.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/gin/ClientModule.java
@@ -1,9 +1,11 @@
 package org.siemac.metamac.access.control.web.client.gin;
 
-import org.siemac.metamac.access.control.web.client.GopestatPlaceManager;
-import org.siemac.metamac.access.control.web.client.NameTokens;
+import org.siemac.metamac.access.control.web.client.AccessControlPlaceManager;
 import org.siemac.metamac.access.control.web.client.presenter.MainPagePresenter;
 import org.siemac.metamac.access.control.web.client.view.MainPageViewImpl;
+
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
 
 public class ClientModule extends AbstractPresenterModule {
 
@@ -15,7 +17,7 @@ public class ClientModule extends AbstractPresenterModule {
 		//	 |_   bind(RootPresenter.class).asEagerSingleton();
 		//	 |_   bind(PlaceManager.class).to(MyPlaceManager.class).in(Singleton.class);
 		//	 |_   bind(GoogleAnalytics.class).to(GoogleAnalyticsImpl.class).in(Singleton.class);		
-	    install(new DefaultModule(GopestatPlaceManager.class));
+	    install(new DefaultModule(AccessControlPlaceManager.class));
 	    
 		// Presenters
 	    bindPresenter(MainPagePresenter.class, MainPagePresenter.MainPageView.class, MainPageViewImpl.class, MainPagePresenter.MainPageProxy.class);
