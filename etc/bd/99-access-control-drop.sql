5f0f280555f4d49a91a0e8e651b8c2c64cda63d9
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-02-19 11:35:13 +0000

    Actualizados scripts
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@33514 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/bd/99-access-control-drop.sql
+++ etc/bd/99-access-control-drop.sql
@@ -21,8 +21,3 @@ DROP TABLE TBL_ROLES CASCADE CONSTRAINTS PURGE;
 -- Drop pk sequence
     
 drop sequence hibernate_sequence;
-
-drop sequence SEQ_ROLES;
-drop sequence SEQ_APPS;
-drop sequence SEQ_USERS;
-drop sequence SEQ_ACCESS;
