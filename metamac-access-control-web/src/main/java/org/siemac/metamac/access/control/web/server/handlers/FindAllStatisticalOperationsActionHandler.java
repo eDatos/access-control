a9c37aa0e7cc6f1af4bdf600934f1919ea806eb0
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-20 12:32:45 +0000

    Llamada a WS de operaciones estadísticas
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@35288 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/FindAllStatisticalOperationsActionHandler.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/FindAllStatisticalOperationsActionHandler.java
@@ -2,11 +2,13 @@ package org.siemac.metamac.access.control.web.server.handlers;
 
 import java.util.List;
 
-import org.siemac.metamac.access.control.web.server.ServiceContextHelper;
+import org.siemac.metamac.access.control.service.ws.StatisticalOperationsInternalWebServiceFacade;
 import org.siemac.metamac.access.control.web.shared.FindAllStatisticalOperationsAction;
 import org.siemac.metamac.access.control.web.shared.FindAllStatisticalOperationsResult;
 import org.siemac.metamac.core.common.dto.ExternalItemBtDto;
-import org.siemac.metamac.core.common.serviceapi.MetamacCoreCommonService;
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
+import org.siemac.metamac.web.common.shared.exception.MetamacWebException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.gwtplatform.dispatch.server.ExecutionContext;
@@ -16,7 +18,7 @@ import com.gwtplatform.dispatch.shared.ActionException;
 public class FindAllStatisticalOperationsActionHandler extends AbstractActionHandler<FindAllStatisticalOperationsAction, FindAllStatisticalOperationsResult> {
 
     @Autowired
-    private MetamacCoreCommonService metamacCoreCommonService;
+    private StatisticalOperationsInternalWebServiceFacade statisticalOperationsInternalWebServiceFacade;
 
     public FindAllStatisticalOperationsActionHandler() {
         super(FindAllStatisticalOperationsAction.class);
@@ -24,8 +26,12 @@ public class FindAllStatisticalOperationsActionHandler extends AbstractActionHan
 
     @Override
     public FindAllStatisticalOperationsResult execute(FindAllStatisticalOperationsAction action, ExecutionContext context) throws ActionException {
-        List<ExternalItemBtDto> operations = metamacCoreCommonService.findAllStatisticalOperations(ServiceContextHelper.getServiceContext());
-        return new FindAllStatisticalOperationsResult(operations);
+        try {
+            List<ExternalItemBtDto> operations = statisticalOperationsInternalWebServiceFacade.findOperations();
+            return new FindAllStatisticalOperationsResult(operations);
+        } catch (MetamacException e) {
+            throw new MetamacWebException(WebExceptionUtils.getMetamacWebExceptionItem(e.getExceptionItems()));
+        }
     }
 
     @Override
