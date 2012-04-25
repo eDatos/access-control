6346e2e388e1a2cd354ad13e32a6631a8205e266
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-25 07:31:11 +0000

    Añadir rol de Administrador a ServiceContext
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@35410 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/ServiceContextHelper.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/ServiceContextHelper.java
@@ -1,6 +1,11 @@
 package org.siemac.metamac.access.control.web.server;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.siemac.metamac.access.control.constants.AccessControlConstants;
+import org.siemac.metamac.domain.access.control.enume.domain.AccessControlRoleEnum;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+import org.siemac.metamac.sso.client.SsoClientConstants;
 
 public class ServiceContextHelper {
 
@@ -8,8 +13,14 @@ public class ServiceContextHelper {
 
     public static ServiceContext getServiceContext() {
         if (serviceContext == null) {
-            serviceContext = new ServiceContext("user", "12345", "metamac");
-            serviceContext.setProperty("locale", "en");
+
+            serviceContext = new ServiceContext("user", "12345", AccessControlConstants.SECURITY_APPLICATION_ID);
+
+            MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
+            metamacPrincipal.setUserId(serviceContext.getUserId());
+            metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(AccessControlRoleEnum.ADMINISTRADOR.getName(), AccessControlConstants.SECURITY_APPLICATION_ID, null));
+            serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
+
         }
         return serviceContext;
     }
