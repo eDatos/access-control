4070c3866f57279e46857192f1bf22fbee0d0fb3
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-26 11:33:25 +0000

    Format
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@35489 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/ServiceContextHolder.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/ServiceContextHolder.java
@@ -9,21 +9,21 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class ServiceContextHolder {
 
-    private static String                SESSION_ATTRIBUTE = "_serviceContext_";
+    private static String SESSION_ATTRIBUTE = "_serviceContext_";
 
     public static ServiceContext getCurrentServiceContext() {
-        ServiceContext serviceContext = (ServiceContext)getCurrentRequest().getSession().getAttribute(ServiceContextHolder.SESSION_ATTRIBUTE);
+        ServiceContext serviceContext = (ServiceContext) getCurrentRequest().getSession().getAttribute(ServiceContextHolder.SESSION_ATTRIBUTE);
         if (serviceContext == null) {
             serviceContext = new ServiceContext("ANNONIMOUS", "NO_SESSION", AccessControlConstants.SECURITY_APPLICATION_ID); // TODO
             putCurrentServiceContext(serviceContext);
         }
         return serviceContext;
     }
-    
+
     public static void putCurrentServiceContext(ServiceContext serviceContext) {
         getCurrentRequest().getSession().setAttribute(SESSION_ATTRIBUTE, serviceContext);
     }
-    
+
     private static HttpServletRequest getCurrentRequest() {
         return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
     }
