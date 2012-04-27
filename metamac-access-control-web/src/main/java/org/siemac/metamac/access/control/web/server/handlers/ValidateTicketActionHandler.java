36cb60050a9227c4e6aebebd1da9620c1e1a7265
jrodped <jrodped@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-27 12:13:38 +0000

    Passed  security ACL to MetamacPrincipal
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@35529 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/ValidateTicketActionHandler.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/ValidateTicketActionHandler.java
@@ -1,21 +1,16 @@
 package org.siemac.metamac.access.control.web.server.handlers;
 
-import javax.net.ssl.HostnameVerifier;
+import javax.annotation.PostConstruct;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.jasig.cas.client.authentication.AttributePrincipal;
-import org.jasig.cas.client.validation.Assertion;
-import org.jasig.cas.client.validation.Saml11TicketValidator;
-import org.jasig.cas.client.validation.TicketValidationException;
 import org.siemac.metamac.access.control.constants.AccessControlConstants;
 import org.siemac.metamac.access.control.web.server.ServiceContextHolder;
 import org.siemac.metamac.access.control.web.shared.ValidateTicketAction;
 import org.siemac.metamac.access.control.web.shared.ValidateTicketResult;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
-import org.siemac.metamac.domain.access.control.enume.domain.AccessControlRoleEnum;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.sso.client.SsoClientConstants;
+import org.siemac.metamac.sso.validation.ValidateTicket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,54 +23,43 @@ import com.gwtplatform.dispatch.shared.ActionException;
 @Component
 public class ValidateTicketActionHandler extends AbstractActionHandler<ValidateTicketAction, ValidateTicketResult> {
 
-    private static Logger        log                        = LoggerFactory.getLogger(ValidateTicketActionHandler.class);
+    protected static Logger      log                        = LoggerFactory.getLogger(ValidateTicketActionHandler.class);
 
     private static String        PROP_CAS_SERVER_URL_PREFIX = "metamac.security.casServerUrlPrefix";
     private static String        PROP_TOLERANCE             = "metamac.security.tolerance";
 
     @Autowired
     private ConfigurationService configurationService       = null;
+    
+    private ValidateTicket validateTicket = null;
 
     public ValidateTicketActionHandler() {
         super(ValidateTicketAction.class);
     }
+    
+    @PostConstruct
+    public void initActionHandler() {
+        String casServerUrlPrefix = configurationService.getConfig().getString(PROP_CAS_SERVER_URL_PREFIX);
+        String tolerance = configurationService.getConfig().getString(PROP_TOLERANCE); // ms
+        
+        validateTicket = new ValidateTicket(casServerUrlPrefix);
+        validateTicket.setTolerance(Long.valueOf(tolerance));
+    }
+    
 
     @Override
     public ValidateTicketResult execute(ValidateTicketAction action, ExecutionContext context) throws ActionException {
-        String casServerUrlPrefix = configurationService.getConfig().getString(PROP_CAS_SERVER_URL_PREFIX);
+        
         String ticket = action.getTicket();
         String service = action.getServiceUrl();
-        String tolerance = configurationService.getConfig().getString(PROP_TOLERANCE); // ms
-        String renew = "false";
-        String encoding = null;
-        // String disableXmlSchemaValidation = "false";
-        HostnameVerifier hostnameVerifier = null; // getHostnameVerifier(filterConfig)
-
-        final Saml11TicketValidator ticketValidator = new Saml11TicketValidator(casServerUrlPrefix);
-        ticketValidator.setTolerance(Long.parseLong(tolerance));
-        ticketValidator.setRenew(parseBoolean(renew));
-        ticketValidator.setHostnameVerifier(hostnameVerifier);
-        ticketValidator.setEncoding(encoding);
-        // validator.setDisableXmlSchemaValidation(parseBoolean(disableXmlSchemaValidation));
-
+        
         try {
-            final Assertion assertion = ticketValidator.validate(ticket, service);
-
-            if (log.isDebugEnabled()) {
-                log.debug("Successfully authenticated user: " + assertion.getPrincipal().getName());
-            }
-
-            AttributePrincipal attributePrincipal = assertion.getPrincipal();
-            ServiceContext serviceContext = new ServiceContext(attributePrincipal.getName(), ticket, AccessControlConstants.SECURITY_APPLICATION_ID);
-            MetamacPrincipal metamacPrincipal = new MetamacPrincipal();
-            metamacPrincipal.setUserId(serviceContext.getUserId());
-            metamacPrincipal.getAccesses().add(new MetamacPrincipalAccess(AccessControlRoleEnum.ADMINISTRADOR.getName(), AccessControlConstants.SECURITY_APPLICATION_ID, null));
+            MetamacPrincipal metamacPrincipal = validateTicket.validateTicket(ticket, service);
+            ServiceContext serviceContext = new ServiceContext(metamacPrincipal.getUserId(), ticket, AccessControlConstants.SECURITY_APPLICATION_ID);
             serviceContext.setProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE, metamacPrincipal);
-
             ServiceContextHolder.putCurrentServiceContext(serviceContext);
-
             return new ValidateTicketResult(metamacPrincipal);
-        } catch (final TicketValidationException e) {
+        } catch (final org.siemac.metamac.sso.exception.TicketValidationException e) {
             throw new ActionException(e);
         }
 
