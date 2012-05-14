934aa09f2a20dfa93240c9ba165bc23cead79a9b
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-14 08:37:49 +0000

    METAMAC-530 - Modificar la configuración de spring
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@35855 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/aop/DisableFlushInterceptor.java
@@ -0,0 +1,15 @@
+package org.siemac.metamac.access.control.aop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.siemac.metamac.core.common.aop.DisableFlushInterceptorBase;
+
+public class DisableFlushInterceptor extends DisableFlushInterceptorBase {
+    
+    @Override
+    @PersistenceContext(unitName = "AccessControlEntityManagerFactory")
+    protected void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+}
