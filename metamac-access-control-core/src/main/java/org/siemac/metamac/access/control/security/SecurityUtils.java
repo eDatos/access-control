d7c9ed6861828c48b7f14ebbd2586c99dca1c274
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-06 06:55:29 +0000

    Seguridad en la web
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@37247 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/security/SecurityUtils.java
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/security/SecurityUtils.java
@@ -1,15 +1,14 @@
 package org.siemac.metamac.access.control.security;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.siemac.metamac.access.control.constants.AccessControlConstants;
 import org.siemac.metamac.access.control.error.ServiceExceptionType;
+import org.siemac.metamac.access.control.security.shared.SharedSecurityUtils;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.domain.access.control.enume.domain.AccessControlRoleEnum;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.sso.client.SsoClientConstants;
 
-public class SecurityUtils {
+public class SecurityUtils extends SharedSecurityUtils {
 
     /**
      * Checks user can execute any operation, if has any role of requested roles
@@ -35,19 +34,6 @@ public class SecurityUtils {
     }
 
     /**
-     * Checks user has requested rol
-     */
-    private static boolean isUserInRol(MetamacPrincipal metamacPrincipal, AccessControlRoleEnum role) throws MetamacException {
-
-        if (AccessControlRoleEnum.ANY_ROLE_ALLOWED.equals(role)) {
-            return isAnyAccessControlRole(metamacPrincipal);
-        } else {
-            return isRoleInAccesses(metamacPrincipal, role);
-        }
-    }
-
-
-    /**
      * Retrieves MetamacPrincipal in ServiceContext
      */
     private static MetamacPrincipal getMetamacPrincipal(ServiceContext ctx) throws MetamacException {
@@ -60,26 +46,6 @@ public class SecurityUtils {
             throw new MetamacException(ServiceExceptionType.SECURITY_PRINCIPAL_NOT_FOUND);
         }
         return metamacPrincipal;
-    }
-
-    /**
-     * Checks if user has access with role
-     */
-    private static Boolean isRoleInAccesses(MetamacPrincipal metamacPrincipal, AccessControlRoleEnum role) {
-        for (MetamacPrincipalAccess metamacPrincipalAccess : metamacPrincipal.getAccesses()) {
-            if (AccessControlConstants.SECURITY_APPLICATION_ID.equals(metamacPrincipalAccess.getApplication()) && metamacPrincipalAccess.getRole().equals(role.name())) {
-                return Boolean.TRUE;
-            }
-        }
-        return Boolean.FALSE;
-    }
-
-    private static Boolean isAnyAccessControlRole(MetamacPrincipal metamacPrincipal) {
-        return isAdministrador(metamacPrincipal) || isRoleInAccesses(metamacPrincipal, AccessControlRoleEnum.TECNICO_PLANIFICACION);
-    }
-
-    private static Boolean isAdministrador(MetamacPrincipal metamacPrincipal) {
-        return isRoleInAccesses(metamacPrincipal, AccessControlRoleEnum.ADMINISTRADOR);
     }
 
 }
---
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/security/shared/SharedSecurityUtils.java
@@ -0,0 +1,42 @@
+package org.siemac.metamac.access.control.security.shared;
+
+import org.siemac.metamac.access.control.constants.AccessControlConstants;
+import org.siemac.metamac.domain.access.control.enume.domain.AccessControlRoleEnum;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+
+public class SharedSecurityUtils {
+
+    /**
+     * Checks user has requested role
+     */
+    public static boolean isUserInRol(MetamacPrincipal metamacPrincipal, AccessControlRoleEnum role) {
+
+        if (AccessControlRoleEnum.ANY_ROLE_ALLOWED.equals(role)) {
+            return isAnyAccessControlRole(metamacPrincipal);
+        } else {
+            return isRoleInAccesses(metamacPrincipal, role);
+        }
+    }
+
+    public static Boolean isAdministrador(MetamacPrincipal metamacPrincipal) {
+        return isRoleInAccesses(metamacPrincipal, AccessControlRoleEnum.ADMINISTRADOR);
+    }
+
+    /**
+     * Checks if user has access with role
+     */
+    public static Boolean isRoleInAccesses(MetamacPrincipal metamacPrincipal, AccessControlRoleEnum role) {
+        for (MetamacPrincipalAccess metamacPrincipalAccess : metamacPrincipal.getAccesses()) {
+            if (AccessControlConstants.SECURITY_APPLICATION_ID.equals(metamacPrincipalAccess.getApplication()) && metamacPrincipalAccess.getRole().equals(role.name())) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    public static Boolean isAnyAccessControlRole(MetamacPrincipal metamacPrincipal) {
+        return isAdministrador(metamacPrincipal) || isRoleInAccesses(metamacPrincipal, AccessControlRoleEnum.TECNICO_PLANIFICACION);
+    }
+
+}
