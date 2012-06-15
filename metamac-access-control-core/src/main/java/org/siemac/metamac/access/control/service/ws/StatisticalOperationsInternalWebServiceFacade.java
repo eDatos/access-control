2dea3526cb6d86aa68492c3a31a4b2bcf3ce5103
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-15 07:53:57 +0000

    WS de operaciones estadísticas paginado
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@37715 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/service/ws/StatisticalOperationsInternalWebServiceFacade.java
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/service/ws/StatisticalOperationsInternalWebServiceFacade.java
@@ -1,15 +1,10 @@
 package org.siemac.metamac.access.control.service.ws;
 
-import java.util.List;
-
-import org.siemac.metamac.core.common.dto.ExternalItemBtDto;
 import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.statistical.operations.internal.ws.v1_0.domain.FindOperationsResult;
 
 public interface StatisticalOperationsInternalWebServiceFacade {
 
-    /**
-     * Find operations and return ExternalItemBtDto 
-     */
-    
-    public List<ExternalItemBtDto> findOperations() throws MetamacException;
+    public FindOperationsResult findOperations(int firstResult, int maxResult) throws MetamacException;
+
 }
--- metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/service/ws/StatisticalOperationsInternalWebServiceFacadeImpl.java
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/service/ws/StatisticalOperationsInternalWebServiceFacadeImpl.java
@@ -1,12 +1,9 @@
 package org.siemac.metamac.access.control.service.ws;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.siemac.metamac.access.control.service.ws.utils.MetamacExceptionUtils;
-import org.siemac.metamac.core.common.dto.ExternalItemBtDto;
-import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.exception.MetamacExceptionItem;
 import org.siemac.metamac.schema.common.v1_0.domain.MetamacCriteria;
@@ -14,8 +11,6 @@ import org.siemac.metamac.schema.common.v1_0.domain.MetamacCriteriaConjunctionRe
 import org.siemac.metamac.schema.common.v1_0.domain.MetamacCriteriaRestrictionList;
 import org.siemac.metamac.statistical.operations.internal.ws.v1_0.MetamacExceptionFault;
 import org.siemac.metamac.statistical.operations.internal.ws.v1_0.domain.FindOperationsResult;
-import org.siemac.metamac.statistical.operations.internal.ws.v1_0.domain.OperationBase;
-import org.siemac.metamac.statistical.operations.internal.ws.v1_0.domain.OperationBaseList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,39 +21,22 @@ public class StatisticalOperationsInternalWebServiceFacadeImpl implements Statis
     private WebServicesLocator webservicesLocator;
 
     @Override
-    public List<ExternalItemBtDto> findOperations() throws MetamacException {
-        OperationBaseList operationBaseList = findOperationsBaseList();
-        
-        List<ExternalItemBtDto> operations = new ArrayList<ExternalItemBtDto>();
-        for (OperationBase item : operationBaseList.getOperation()) {
-            operations.add(new ExternalItemBtDto("http://statisticalOperations/" + item.getCode(), item.getCode(), TypeExternalArtefactsEnum.STATISTICAL_OPERATION));
-        }
-        
-        return operations;
-    }
-    
-    private OperationBase retrieveOperation(String operationCode) throws MetamacException {
-        try {
-            return webservicesLocator.getMetamacStatisticalOperationsInternalInterface().retrieveOperation(operationCode);
-        } catch (MetamacExceptionFault e) {
-            List<MetamacExceptionItem> metamacExceptionItems = MetamacExceptionUtils.getMetamacExceptionItems(e.getFaultInfo().getExceptionItems());
-            throw new MetamacException(metamacExceptionItems);
-        }
-    }
-
-    private OperationBaseList findOperationsBaseList() throws MetamacException {
+    public FindOperationsResult findOperations(int firstResult, int maxResult) throws MetamacException {
         try {
             MetamacCriteria metamacCriteria = new MetamacCriteria();
             metamacCriteria.setRestriction(new MetamacCriteriaConjunctionRestriction());
             ((MetamacCriteriaConjunctionRestriction) metamacCriteria.getRestriction()).setRestrictions(new MetamacCriteriaRestrictionList());
 
-            metamacCriteria.setMaximumResultSize(BigInteger.valueOf(1000));
-            
-            FindOperationsResult findOperationsResult = webservicesLocator.getMetamacStatisticalOperationsInternalInterface().findOperations(metamacCriteria);
-            return findOperationsResult.getOperations();
+            // Pagination
+            metamacCriteria.setFirstResult(BigInteger.valueOf(firstResult));
+            metamacCriteria.setMaximumResultSize(BigInteger.valueOf(maxResult));
+            metamacCriteria.setCountTotalResults(true);
+
+            return webservicesLocator.getMetamacStatisticalOperationsInternalInterface().findOperations(metamacCriteria);
         } catch (MetamacExceptionFault e) {
             List<MetamacExceptionItem> metamacExceptionItems = MetamacExceptionUtils.getMetamacExceptionItems(e.getFaultInfo().getExceptionItems());
             throw new MetamacException(metamacExceptionItems);
         }
     }
+
 }
