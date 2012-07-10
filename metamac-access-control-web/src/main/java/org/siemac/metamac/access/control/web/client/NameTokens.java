cab3fa7a8b1cee144da6092fe35ebeefed3ba7b7
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-10 11:28:06 +0000

    METAMAC-791 metamac-access-control: cambio de formato de los NameTokens
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@38587 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/NameTokens.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/NameTokens.java
@@ -2,11 +2,11 @@ package org.siemac.metamac.access.control.web.client;
 
 public class NameTokens {
 
-    public static final String mainPage               = "!MainPage";
-    public static final String usersListPage          = "!UserListPage";
-    public static final String roleHistoryPage        = "!RoleHistoryPage";
+    public static final String mainPage               = "mainPage";
+    public static final String usersListPage          = "users";
+    public static final String roleHistoryPage        = "roleHistory";
 
-    public static final String errorPage              = "!ErrorPage";
-    public static final String unauthorizedAccessPage = "!UnauthorizedAccessPage";
+    public static final String errorPage              = "error";
+    public static final String unauthorizedAccessPage = "unauthorizedAccess";
 
 }
