0f3d5634392748aa3baca517272140a812b7d6bb
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-12 12:58:45 +0000

    METAMAC-1298 Scripts SQL para ORACLE: función para asignar secuencias
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@41726 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/db/oracle/01-create/ejemplos/05-access-control-initial-import-admin.sql
+++ etc/db/oracle/01-create/ejemplos/05-access-control-initial-import-admin.sql
@@ -11,10 +11,8 @@
 
 -- Ejemplo de creación de un usuario:
 Insert into TB_USERS (ID, USERNAME, UUID, NAME, SURNAME, MAIL, VERSION, CREATED_BY, CREATED_DATE, CREATED_DATE_TZ, LAST_UPDATED_BY, LAST_UPDATED, LAST_UPDATED_TZ) values 
-(1,'METAMAC_ADMIN','94e714ea-d168-4d32-b3f4-f4097029a400','Administrador METAMAC', 'Administrador METAMAC', 'administrador-metamac@gobiernodecanarias.org', 1, 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London', 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London');
+(GET_NEXT_SEQUENCE_VALUE('USERS'), 'METAMAC_ADMIN','94e714ea-d168-4d32-b3f4-f4097029a400','Administrador METAMAC', 'Administrador METAMAC', 'administrador-metamac@gobiernodecanarias.org', 1, 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London', 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London');
 
--- Actualización de sequencias:
-Insert into TB_SEQUENCES(SEQUENCE_NAME, SEQUENCE_NEXT_VALUE) VALUES ('USERS', 2);
 
 -- ------------------------
 -- TB_ACCESS INITIAL IMPORT
@@ -30,10 +28,13 @@ Insert into TB_SEQUENCES(SEQUENCE_NAME, SEQUENCE_NEXT_VALUE) VALUES ('USERS', 2)
 
 -- Ejemplo de creación de un acceso:
 Insert into TB_ACCESS (ID, UUID, ROLE_FK, APP_FK, USER_FK, VERSION, CREATED_BY, CREATED_DATE, CREATED_DATE_TZ, LAST_UPDATED_BY, LAST_UPDATED, LAST_UPDATED_TZ) values 
-(1, '1fa6695a-0fb0-40c6-bf77-b3198ca60a5f','1', '1', 1, 1, 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London', 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London');
+(GET_NEXT_SEQUENCE_VALUE('ACCESS'), '1fa6695a-0fb0-40c6-bf77-b3198ca60a5f', '1', '1', GET_NEXT_SEQUENCE_VALUE('USERS'), 1, 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London', 'access_control_app', to_date('2012-04-19 09:00:00', 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London');
+
 
 -- Actualización de sequencias:
-Insert into TB_SEQUENCES(SEQUENCE_NAME, SEQUENCE_NEXT_VALUE) VALUES ('ACCESS', 2);
+
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'USERS';
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'ACCESS';
 
 
 -- ------------------------
