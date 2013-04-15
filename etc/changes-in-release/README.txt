1ae048ff7f5cd533a4195b720ec9295403d4258a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-15 11:07:18 +0000

    METAMAC-1564 Listener para comprobar propiedades de configuración del data
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@45119 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-in-release/README.txt
+++ etc/changes-in-release/README.txt
@@ -5,5 +5,8 @@ Cuando se cree la RELEASE, añadir estos pasos al manual de instalación:
 2. Base de datos:
 	- Ejecutar el script 03-updates-in-release/01-update-columns-length.sql, tanto para Oracle como MSSql (Cambiado tamaño de Enums y otra columna de ExternalItem)
 	- Ejecutar el script 03-updates-in-release/02-update-localised-string.sql, tanto para Oracle como MSSql
-	
+
+3. DATA:
+	- Eliminada propiedad del DATA 'metamac.access.control.clients.statistical.operations.rest.internal' (ya se establece en el DATA común)	
+
 99. Reiniciar Tomcat
---
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/constants/AccessControlConfigurationConstants.java
@@ -0,0 +1,18 @@
+package org.siemac.metamac.access.control.constants;
+
+public class AccessControlConfigurationConstants {
+
+    // PROPERTIES SPECIFIED IN THE DATA DIRECTORY
+
+    // Configuration
+
+    public static final String USER_GUIDE_FILE_NAME = "metamac.access.control.user.guide.file.name";
+
+    // DataSource
+
+    public static final String DB_URL               = "metamac.access.control.db.url";
+    public static final String DB_USERNAME          = "metamac.access.control.db.username";
+    public static final String DB_PASSWORD          = "metamac.access.control.db.password";
+    public static final String DB_DIALECT           = "metamac.access.control.db.dialect";
+    public static final String DB_DRIVER_NAME       = "metamac.access.control.db.driverName";
+}
