88023989ac24909f30d6775a44c9963028db6c9d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-05 10:25:46 +0000

    METAMAC-916 Integración con API de operaciones estadísticas
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@46458 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/GetOperationPaginatedListActionHandler.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/GetOperationPaginatedListActionHandler.java
@@ -3,18 +3,14 @@ package org.siemac.metamac.access.control.web.server.handlers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.access.control.web.server.rest.StatisticalOperationsRestInternalFacade;
+import org.siemac.metamac.access.control.web.server.rest.utils.ExternalItemUtils;
 import org.siemac.metamac.access.control.web.shared.GetOperationPaginatedListAction;
 import org.siemac.metamac.access.control.web.shared.GetOperationPaginatedListResult;
-import org.siemac.metamac.core.common.conf.ConfigurationService;
-import org.siemac.metamac.core.common.constants.shared.ConfigurationConstants;
 import org.siemac.metamac.core.common.dto.ExternalItemDto;
-import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
-import org.siemac.metamac.rest.common.v1_0.domain.Resource;
 import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Operations;
+import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.ResourceInternal;
 import org.siemac.metamac.web.common.server.handlers.SecurityActionHandler;
-import org.siemac.metamac.web.common.server.utils.DtoUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,17 +22,12 @@ public class GetOperationPaginatedListActionHandler extends SecurityActionHandle
     @Autowired
     private StatisticalOperationsRestInternalFacade statisticalOperationsRestInternalFacade;
 
-    @Autowired
-    private ConfigurationService                    configurationService;
-
     public GetOperationPaginatedListActionHandler() {
         super(GetOperationPaginatedListAction.class);
     }
 
     @Override
     public GetOperationPaginatedListResult executeSecurityAction(GetOperationPaginatedListAction action) throws ActionException {
-
-        String operationtsApiEndpoint = configurationService.getProperty(ConfigurationConstants.ENDPOINT_STATISTICAL_OPERATIONS_INTERNAL_API);
 
         int firstResult = 0;
         int totalResults = 0;
@@ -45,12 +36,8 @@ public class GetOperationPaginatedListActionHandler extends SecurityActionHandle
         if (result != null && result.getOperations() != null) {
             firstResult = result.getOffset().intValue();
             totalResults = result.getTotal().intValue();
-            for (Resource resource : result.getOperations()) {
-                // Do not store rest api endpoint
-                String uri = StringUtils.removeStart(resource.getSelfLink().getHref(), operationtsApiEndpoint);
-
-                ExternalItemDto externalItemDto = new ExternalItemDto(resource.getId(), uri, resource.getUrn(), null, TypeExternalArtefactsEnum.STATISTICAL_OPERATION,
-                        DtoUtils.getInternationalStringDtoFromInternationalString(resource.getTitle()));
+            for (ResourceInternal resource : result.getOperations()) {
+                ExternalItemDto externalItemDto = ExternalItemUtils.getExternalItemDtoFromStatisticalOperationsResourceInternal(resource);
                 externalItemDtos.add(externalItemDto);
             }
         }
