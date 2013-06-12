8ab69c14e7c7f2b0ec1f9e0e3b37c3898351552a
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-12 12:36:21 +0000

    Refactor
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@46730 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/SaveUserActionHandler.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/server/handlers/SaveUserActionHandler.java
@@ -11,7 +11,6 @@ import org.siemac.metamac.web.common.server.utils.WebExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.shared.ActionException;
 
 @Component
@@ -39,10 +38,4 @@ public class SaveUserActionHandler extends SecurityActionHandler<SaveUserAction,
             throw WebExceptionUtils.createMetamacWebException(e);
         }
     }
-
-    @Override
-    public void undo(SaveUserAction action, SaveUserResult result, ExecutionContext context) throws ActionException {
-
-    }
-
 }
