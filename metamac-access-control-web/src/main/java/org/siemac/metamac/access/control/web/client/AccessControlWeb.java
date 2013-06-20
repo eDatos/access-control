bc633dc01e8fb1fbbcf5149b087db67766b6cbc1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-20 06:20:43 +0000

    Refactor
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@46982 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/AccessControlWeb.java
+++ metamac-access-control-web/src/main/java/org/siemac/metamac/access/control/web/client/AccessControlWeb.java
@@ -1,34 +1,12 @@
 package org.siemac.metamac.access.control.web.client;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.siemac.metamac.access.control.constants.AccessControlConstants;
 import org.siemac.metamac.access.control.web.client.gin.AccessControlWebGinjector;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
 import org.siemac.metamac.web.common.client.MetamacSecurityEntryPoint;
-import org.siemac.metamac.web.common.client.events.LoginAuthenticatedEvent;
 import org.siemac.metamac.web.common.client.gin.MetamacWebGinjector;
-import org.siemac.metamac.web.common.client.utils.ApplicationEditionLanguages;
-import org.siemac.metamac.web.common.client.utils.ApplicationOrganisation;
-import org.siemac.metamac.web.common.client.widgets.WaitingAsyncCallback;
-import org.siemac.metamac.web.common.shared.GetLoginPageUrlAction;
-import org.siemac.metamac.web.common.shared.GetLoginPageUrlResult;
-import org.siemac.metamac.web.common.shared.LoadConfigurationPropertiesAction;
-import org.siemac.metamac.web.common.shared.LoadConfigurationPropertiesResult;
-import org.siemac.metamac.web.common.shared.MockCASUserAction;
-import org.siemac.metamac.web.common.shared.MockCASUserResult;
-import org.siemac.metamac.web.common.shared.ValidateTicketAction;
-import org.siemac.metamac.web.common.shared.ValidateTicketResult;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.http.client.UrlBuilder;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.CssResource.NotStrict;
-import com.google.gwt.user.client.Window;
-import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -36,8 +14,6 @@ import com.gwtplatform.mvp.client.DelayedBindRegistry;
 public class AccessControlWeb extends MetamacSecurityEntryPoint {
 
     private static final Boolean                  SECURITY_ENABLED = true;
-
-    private static Logger                         logger           = Logger.getLogger(AccessControlWeb.class.getName());
 
     private static MetamacPrincipal               principal;
     private static AccessControlWebConstants      constants;
@@ -48,7 +24,7 @@ public class AccessControlWeb extends MetamacSecurityEntryPoint {
     @Override
     public void onModuleLoad() {
         setUncaughtExceptionHandler();
- 
+
         prepareApplication(SECURITY_ENABLED);
     }
 
@@ -73,32 +49,29 @@ public class AccessControlWeb extends MetamacSecurityEntryPoint {
     public static AccessControlWebGinjector getAccessControlWebGinjector() {
         return ginjector;
     }
-    
+
     @Override
     protected String getApplicationTitle() {
         return getConstants().appTitle();
     }
-    
+
     @Override
     protected MetamacPrincipal getPrincipal() {
         return principal;
     }
-    
+
     @Override
     protected void setPrincipal(MetamacPrincipal principal) {
-        this.principal = principal;
+        AccessControlWeb.principal = principal;
     }
-    
+
     @Override
     protected String getSecurityApplicationId() {
         return AccessControlConstants.SECURITY_APPLICATION_ID;
     }
-    
+
     @Override
     protected MetamacWebGinjector getWebGinjector() {
         return getAccessControlWebGinjector();
     }
-    
-    
-    
 }
