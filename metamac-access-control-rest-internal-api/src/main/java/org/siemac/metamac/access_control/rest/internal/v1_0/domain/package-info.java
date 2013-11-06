804a2ae6dc43981da9acaf8d1c5617375766390b
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-06 12:40:35 +0000

    METAMAC-1968: Api Rest de Acces-Control http://jira.arte-consultores.com/browse/METAMAC-1968
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@49844 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/access_control/rest/internal/v1_0/domain/package-info.java
@@ -0,0 +1,3 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/access-control/v1.0/domain", xmlns = {@javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/access-control/v1.0/domain", prefix = "stat")})
+package org.siemac.metamac.access_control.rest.internal.v1_0.domain;
+
---
+++ metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/access_control/rest/internal/v1_0/service/AccessControlRestInternalFacadeV1_0.java
@@ -0,0 +1,9 @@
+package org.siemac.metamac.access_control.rest.internal.v1_0.service;
+
+import javax.ws.rs.Path;
+
+@Path("v1.0")
+// IMPORTANT: If a new version of API is added, remember change latest url y urlrewrite.xml in war
+public interface AccessControlRestInternalFacadeV1_0 {
+
+}
