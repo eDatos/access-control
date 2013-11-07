33bb8a65ba8f2c04448c93c41e46641c44f44387
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-07 12:36:57 +0000

    METAMAC-1968: Api Rest de Acces-Control http://jira.arte-consultores.com/browse/METAMAC-1968
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@49881 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/access_control/rest/internal/v1_0/service/AccessControlRestInternalFacadeV1_0.java
+++ metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/access_control/rest/internal/v1_0/service/AccessControlRestInternalFacadeV1_0.java
@@ -1,9 +1,18 @@
 package org.siemac.metamac.access_control.rest.internal.v1_0.service;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.siemac.metamac.rest.access_control.v1_0.domain.Users;
 
 @Path("v1.0")
 // IMPORTANT: If a new version of API is added, remember change latest url y urlrewrite.xml in war
 public interface AccessControlRestInternalFacadeV1_0 {
 
+    @GET
+    @Produces({"application/xml"})
+    @Path("users")
+    Users findUsers(@QueryParam("query") String query, @QueryParam("limit") String limit, @QueryParam("offset") String offset);
 }
--- metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/rest/access_control/v1_0/domain/package-info.java
+++ metamac-access-control-rest-internal-api/src/main/java/org/siemac/metamac/rest/access_control/v1_0/domain/package-info.java
@@ -1,3 +1,3 @@
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/access-control/v1.0/domain", xmlns = {@javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/access-control/v1.0/domain", prefix = "stat")})
-package org.siemac.metamac.access_control.rest.internal.v1_0.domain;
+package org.siemac.metamac.rest.access_control.v1_0.domain;
 
