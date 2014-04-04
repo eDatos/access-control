b32aefab96b2c0bb170b81af9396b91965966f64
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-04-04 09:07:22 +0000

    METAMAC-2250 - [CORE] Unificar gestión de APPLICATION_ID >> access-control: revert
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@51910 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/constants/AccessControlConstants.java
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/constants/AccessControlConstants.java
@@ -1,11 +1,9 @@
 package org.siemac.metamac.access.control.constants;
 
-import org.siemac.metamac.rest.notices.v1_0.domain.enume.MetamacApplicationsEnum;
-
 public final class AccessControlConstants {
 
     private AccessControlConstants() {
     }
 
-    public static final String APPLICATION_ID = MetamacApplicationsEnum.GESTOR_ACCESOS.getName();
+    public static final String APPLICATION_ID = "GESTOR_ACCESOS";
 }
