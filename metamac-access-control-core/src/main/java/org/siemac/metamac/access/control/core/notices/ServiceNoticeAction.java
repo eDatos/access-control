fd50e26cb0c638dd083ef2b93c18f43d67ff85cc
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-05-14 08:16:01 +0000

    METAMAC-1995 - Añadir notificaciones al gestor de accesos http://jira.arte-consultores.com/browse/METAMAC-1995
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-access-control/trunk/metamac-access-control@52165 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/core/notices/ServiceNoticeAction.java
@@ -0,0 +1,7 @@
+package org.siemac.metamac.access.control.core.notices;
+
+public class ServiceNoticeAction {
+
+    public static final String ACCESS_CREATED = "notice_message.acl.action.access_created";
+    public static final String ACCESS_DELETED = "notice_message.acl.action.access_deleted";
+}
---
+++ metamac-access-control-core/src/main/java/org/siemac/metamac/access/control/core/notices/ServiceNoticeMessage.java
@@ -0,0 +1,7 @@
+package org.siemac.metamac.access.control.core.notices;
+
+public class ServiceNoticeMessage {
+
+    public static final String ACCESS_CREATED_OK = "notice_message.acl.message.access_created.ok";
+    public static final String ACCESS_DELETED_OK = "notice_message.acl.message.access_deleted.ok";
+}
