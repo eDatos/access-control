3eff33230f046f19eeadeb84724dc07a6eeb5131
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2016-08-10 11:31:14 +0100

    METAMAC-2475 - Migración de las aplicaciones de Tomcat6 a Tomcat8 y JDK8
    - Deploy

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -1,38 +1,36 @@
 #!/bin/sh
 
-TMP_PATH=/servers/metamac/tmp
+HOME_PATH=metamac-access-control-web
+TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
 
-scp -r etc/deploy deploy@estadisticas.arte-consultores.com:~/metamac-access-control-web:
-scp metamac-access-control-web/target/access-control-internal-*.war deploy@estadisticas.arte-consultores.com:~/metamac-access-control-web/access-control-internal.war
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp metamac-access-control-web/target/access-control-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/access-control-internal.war
 ssh deploy@estadisticas.arte-consultores.com <<EOF
 
-    chmod a+x deploy/*.sh;
-    . deploy/utilities.sh
-    
+    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+    . $TRANSFER_PATH/deploy/utilities.sh
+
     sudo service metamac01 stop
     checkPROC "metamac"
-    
-    
+
+
     ###
     # ACCES-CONTROL-INTERNAL
     ###
-    # Backup Configuration
-    sudo mv $DEPLOY_TARGET_PATH/access-control-internal/$ENVIRONMENT_RELATIVE_PATH_FILE $TMP_PATH/environment.xml_access-control-internal_tmp
-    sudo mv $DEPLOY_TARGET_PATH/access-control-internal/$LOGBACK_RELATIVE_PATH_FILE $TMP_PATH/logback.xml_access-control-internal_tmp
-    
+
     # Update Process
-    sudo mv metamac-access-control-web/access-control-internal.war $DEPLOY_TARGET_PATH/access-control-internal.war
-    sudo unzip $DEPLOY_TARGET_PATH/access-control-internal.war -d $DEPLOY_TARGET_PATH
+    sudo rm -rf $DEPLOY_TARGET_PATH/access-control-internal
+    sudo mv $TRANSFER_PATH/access-control-internal.war $DEPLOY_TARGET_PATH/access-control-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH/access-control-internal.war -d $DEPLOY_TARGET_PATH/access-control-internal
     sudo rm -rf $DEPLOY_TARGET_PATH/access-control-internal.war
-    
+
     # Restore Configuration
-    sudo mv $TMP_PATH/environment.xml_access-control-internal_tmp $DEPLOY_TARGET_PATH/access-control-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo mv $TMP_PATH/logback.xml_access-control-internal_tmp $DEPLOY_TARGET_PATH/access-control-internal/$LOGBACK_RELATIVE_PATH_FILE
-    
-    
+    sudo mv $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/access-control-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo mv $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/access-control-internal/$LOGBACK_RELATIVE_PATH_FILE
+
     sudo chown -R metamac.metamac /servers/metamac
     sudo service metamac01 start
     checkURL "http://estadisticas.arte-consultores.com/access-control-internal" "metamac01"
