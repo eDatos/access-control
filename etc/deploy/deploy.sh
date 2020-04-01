d0e06b9e728e4bbeb283d4d0301bff615c2e065a
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-04-01 13:31:25 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Modificaciones en el fichero deploy.sh propuesta durante la revisión del merge request

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 HOME_PATH=metamac-access-control-web
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH_INTERNAL=/servers/edatos-internal/tomcats/edatos-internal01/webapps
+DEPLOY_TARGET_PATH=/servers/edatos-internal/tomcats/edatos-internal01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
 RESTART=1
@@ -20,7 +20,7 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     . $TRANSFER_PATH/deploy/utilities.sh
 
     ###
-    # ACCES-CONTROL-INTERNAL
+    # ACCESS-CONTROL-INTERNAL
     ###
 
     if [ $RESTART -eq 1 ]; then
@@ -29,21 +29,20 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     fi
 
     # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal
-    sudo mv $TRANSFER_PATH/access-control-internal.war $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal.war
-    sudo unzip $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal.war -d $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal
-    sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal.war
+    sudo rm -rf $DEPLOY_TARGET_PATH/access-control-internal
+    sudo mv $TRANSFER_PATH/access-control-internal.war $DEPLOY_TARGET_PATH/access-control-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH/access-control-internal.war -d $DEPLOY_TARGET_PATH/access-control-internal
+    sudo rm -rf $DEPLOY_TARGET_PATH/access-control-internal.war
 
     # Restore Configuration
-    sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH_INTERNAL/access-control-internal/$LOGBACK_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/access-control-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/access-control-internal/$LOGBACK_RELATIVE_PATH_FILE
 
     if [ $RESTART -eq 1 ]; then
         sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal
         sudo service edatos-internal01 start
     fi
 
-    # checkURL "http://estadisticas.arte-consultores.com/access-control-internal" "metamac01"
     echo "Finished deploy"
 
 EOF
