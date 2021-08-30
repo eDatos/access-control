ea13a506dd40099d973d4074f21852cf9cb4b273
Carlos Iván Plasencia Santana <cplasan@arte-consultores.com>
2021-08-30 07:45:24 +0100

    EDATOS-3411 - COETL Usar control de usuarios del ISTAC en la autenticacion
    > Se añade la aplicación de COETL a la importación de datos

--- etc/changes-from-release/2.5.1/db/access-control/postgresql/20210604_added_new_rol_and_app.sql
+++ etc/changes-from-release/2.5.1/db/access-control/postgresql/20210604_added_new_rol_and_app.sql
@@ -10,4 +10,12 @@ INSERT INTO TB_APPS (ID, CODE, UUID, TITLE, DESCRIPTION, VERSION, CREATED_BY, CR
 ((SELECT GET_NEXT_SEQUENCE_VALUE('APPS')), 'GESTOR_USUARIOS_EXTERNOS','4cb47e26-5cda-480e-9ce5-4556e78441b3','Gestor de usuarios externos', 'Aplicativo encargado de gestionar los usuarios externos', 1, 'access_control_app', current_timestamp, 'Europe/London', 'access_control_app', current_timestamp, 'Europe/London');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'APPS';
 
+-- --------------------------------------------------------------------------------------------------
+-- EDATOS-3411 - Usar autenticación del ISTAC (CoETL)
+-- --------------------------------------------------------------------------------------------------
+
+INSERT INTO TB_APPS (ID, CODE, UUID, TITLE, DESCRIPTION, VERSION, CREATED_BY, CREATED_DATE, CREATED_DATE_TZ, LAST_UPDATED_BY, LAST_UPDATED, LAST_UPDATED_TZ) VALUES
+(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('APPS'), 'GESTOR_CONSOLA_ETL','cdbb5a10-78db-4454-9a45-933c7563fc8b','Gestor de consola de ETLs', 'Aplicativo encargado de gestionar y ejecutar las ETLs', 1, 'access_control_app', current_timestamp, 'Europe/London', 'access_control_app', current_timestamp, 'Europe/London');
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'APPS';
+
 commit;
